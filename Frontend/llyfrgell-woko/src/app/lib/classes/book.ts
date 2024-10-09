3d10156b353f3313d3a65614d89a909ce8d74c3a
Bradley Watkins <watkinsbradley01@gmail.com>
2024-10-09 10:19:25 +0100

    Merge and ignore .env

---
+++ Frontend/llyfrgell-woko/src/app/lib/classes/book.ts
@@ -0,0 +1,11 @@
+export type Book = {
+    id?: number,
+    title: string,
+    author: string, 
+    genre: string,
+    isbn?: string | null, 
+    dateObtained?: Date | null, 
+    dateCompleted?: Date | null, 
+    dateStartedReading?: Date | null, 
+    considerTowardsTotalBooksCompleted: boolean, 
+}
