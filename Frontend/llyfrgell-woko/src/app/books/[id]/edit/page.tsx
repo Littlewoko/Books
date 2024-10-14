5d6661176d8c637a49f04b62f9dcf5d4fa1fc8d3
Bradley Watkins <watkinsbradley01@gmail.com>
2024-10-14 11:02:10 +0100

    protected create and edit routes

--- Frontend/llyfrgell-woko/src/app/books/[id]/edit/page.tsx
+++ Frontend/llyfrgell-woko/src/app/books/[id]/edit/page.tsx
@@ -2,22 +2,29 @@ import Form from "@/app/ui/books/edit-form";
 import { fetchBookById } from "@/app/lib/books/data";
 import { getDummyBooks } from "@/app/lib/dummy/books";
 import { Book } from "@/app/lib/classes/book";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
 
-export default async function Page({ params }: { params: { id: string }}) {
-    const id = params.id;
-    
-    const useDatabase = false;
-    let book: Book | undefined;
-    if(useDatabase) {
-        book = await fetchBookById(id);
-    } else {
-        const books = getDummyBooks();
-        book = books.find(x => x.id?.toString() == id);
-    }
-
-    return (
-      <main>
-        <Form book={book} />
-      </main>
-    );
+export default async function Page({ params }: { params: { id: string } }) {
+  const session = await getServerSession();
+  if(!session || !session.user) {
+    redirect("/api/auth/signin");
   }
+
+  const id = params.id;
+
+  const useDatabase = false;
+  let book: Book | undefined;
+  if (useDatabase) {
+    book = await fetchBookById(id);
+  } else {
+    const books = getDummyBooks();
+    book = books.find(x => x.id?.toString() == id);
+  }
+
+  return (
+    <main>
+      <Form book={book} />
+    </main>
+  );
+}
--- Frontend/llyfrgell-woko/src/app/books/create/page.tsx
+++ Frontend/llyfrgell-woko/src/app/books/create/page.tsx
@@ -1,6 +1,13 @@
 import Form from "@/app/ui/books/create-form"
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
 
 export default async function Page() {
+    const session = await getServerSession();
+    if (!session || !session.user) {
+      redirect("/api/auth/signin");
+    }
+
     return (
         <main>
             <Form />
