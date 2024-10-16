e3832fd5b19d4aee1b236a7f649a8b68e0533efb
Bradley Watkins <watkinsbradley01@gmail.com>
2024-10-16 09:36:44 +0100

    updated home screen text

--- Frontend/llyfrgell-woko/src/app/page.tsx
+++ Frontend/llyfrgell-woko/src/app/page.tsx
@@ -1,7 +1,12 @@
 export default async function Home() {
   return (
     <div>
-      Welcome to my library! You&apos;ll have to bring your own books.
+      <h2 className="text-3xl font-bold dark:text-amber-700">
+        Quote of the day
+      </h2>
+      <p className="my-4 text-lg text-gray-500 dark:text-amber-500">
+        A life without books is a life not lived. - Jay Kristoff
+      </p>
     </div>
   )
 }
