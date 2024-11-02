faae53bd60110db047c25f5e0fa0c9256f0da5b1
Bradley Watkins <watkinsbradley01@gmail.com>
2024-11-02 13:02:15 +0000

    updated home logo icon

--- Frontend/llyfrgell-woko/src/app/ui/desktopNavLinks.tsx
+++ Frontend/llyfrgell-woko/src/app/ui/desktopNavLinks.tsx
@@ -8,14 +8,6 @@ export function DesktopNavLinks() {
   return (
     <ul className="flex flex-row gap-2">
       <li className="text-xl hover:text-blue-900 hover:font-semibold">
-        <Link href="/">
-          <button type="button" className="flex items-center text-white bg-gradient-to-r from-black to-gray-400 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-small rounded-lg text-sm p-1 px-2 md:px-3 text-center text-xs md:text-sm">
-            <HomeIcon className="md:mr-1" fontSize="small" />
-            <span className="hidden md:inline">Adre</span>
-          </button>
-        </Link>
-      </li>
-      <li className="text-xl hover:text-blue-900 hover:font-semibold">
         <Link href="/books">
           <button type="button" className="flex items-center text-white bg-gradient-to-r from-black to-gray-400 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-small rounded-lg text-sm p-1 px-2 md:px-3 text-center text-xs md:text-sm">
             <LibraryBooksIcon className="md:mr-1" fontSize="small" />
--- Frontend/llyfrgell-woko/src/app/ui/logoLink.tsx
+++ Frontend/llyfrgell-woko/src/app/ui/logoLink.tsx
@@ -1,9 +1,14 @@
 import Link from 'next/link';
+import HomeIcon from '@mui/icons-material/Home';
+
 export function LogoLink() {
   return (
-    <div className="mb-4 text-center sm:mb-0">
+    <div className="pl-2 text-center sm:mb-0 flex-grow">
       <Link href="/">
-        <div className="bg-black h-10 w-10"></div>
+        <button type="button" className="flex items-center text-white bg-gradient-to-r from-black to-gray-400 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-small rounded-lg text-sm p-1 px-2 md:px-3 text-center text-xs md:text-sm">
+          <HomeIcon className="md:mr-1" fontSize="small" />
+          <span className="hidden md:inline">Adre</span>
+        </button>
       </Link>
     </div>
   );
