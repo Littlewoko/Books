2bdde09a81cc2d59c39d52aa4074cb345a8b0ddf
Bradley Watkins <watkinsbradley01@gmail.com>
2024-11-02 12:53:53 +0000

    menu icon update

--- Frontend/llyfrgell-woko/src/app/ui/mobileDrawer.tsx
+++ Frontend/llyfrgell-woko/src/app/ui/mobileDrawer.tsx
@@ -17,8 +17,8 @@ const MobileDrawer: React.FC<Props> = ({ isOpen, onClose }) => {
       className={`fixed flex flex-col justify-center items-center z-10 top-0 right-0 h-full w-full bg-white text-black transition-transform duration-300 transform ${isOpen ? 'translate-x-0' : 'translate-x-full'
         }`}
     >
-      <button className="absolute right-4 top-4 p-3" onClick={onClose}>
-        <FontAwesomeIcon className="text-3xl" icon={faXmark} />
+      <button className="absolute right-4 top-4 p-3 text-black" onClick={onClose}>
+        <FontAwesomeIcon className="text-3xl text-black" icon={faXmark}  />
       </button>
       <ul className="flex flex-col justify-center items-center space-y-4">
         <li className="text-xl hover:text-blue-900 hover:font-semibold">
--- Frontend/llyfrgell-woko/src/app/ui/mobileMenuButton.tsx
+++ Frontend/llyfrgell-woko/src/app/ui/mobileMenuButton.tsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { Menu } from '@mui/icons-material';
 
 interface Props {
     onClick: () => void;
@@ -8,7 +9,7 @@ interface Props {
 const MobileMenuButton: React.FC<Props> = ({ onClick }) => {
   return (
     <button className="mr-4 pb-2" onClick={onClick}>
-      <FontAwesomeIcon className="text-3xl" icon={faBars} />
+      <Menu className="text-3xl" sx={{color: "black"}}/>
     </button>
   );
 }
