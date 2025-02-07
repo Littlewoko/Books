f7df3a2635ad9c60496a8af215bb2bc8df0ddb23
Bradley Watkins <watkinsbradley01@gmail.com>
2025-02-07 16:37:35 +0000

    moving between pages

--- Frontend/llyfrgell-woko/src/app/ui/paging.tsx
+++ Frontend/llyfrgell-woko/src/app/ui/paging.tsx
@@ -1,10 +1,19 @@
 interface Props {
     page: number, 
-    setPage: React.Dispatch<React.SetStateAction<number>>
+    setPage: React.Dispatch<React.SetStateAction<number>>,
+    pageLimit: number
 }
 
-export default function Paging({ page, setPage }: Props) {
+export default function Paging({ page, setPage, pageLimit = 2 }: Props) {
+    const decrement = () => {
+        if(page == 0) return;
+
+        setPage(page - 1);
+    }
+    
     const increment = () => {
+        if(page === pageLimit) return;
+
         setPage(page + 1);
     }
 
@@ -16,27 +25,49 @@ export default function Paging({ page, setPage }: Props) {
         return "relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex";
     }
 
+    const pageNumbers = (): number[] => {
+        const actualPage = page + 1;
+
+        if (pageLimit < 2) {
+            // If there are less than three pages, return all available pages
+            return Array.from({ length: pageLimit }, (_, index) => index + 1);
+        }
+    
+        if (actualPage === 1) {
+            // If the current page is the first page, return the first three pages
+            return [1, 2, 3];
+        }
+    
+        if (actualPage === pageLimit + 1) {
+            // If the current page is the last page, return the last three pages
+            return [actualPage - 2, actualPage - 1, actualPage];
+        }
+    
+        // For all other cases, return the current page and its adjacent pages
+        return [actualPage - 1, actualPage, actualPage + 1];
+    }
+
+    const pageNumberDisplay = () => {
+        let arr = pageNumbers();
+
+        return arr.map(num => <a className={getPageNumberCSS(num)}>{num}</a>)
+    }
+
     return (
         <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-            <div className="flex flex-1 justify-between sm:hidden">
-                <a href="#" className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</a>
-                <a href="#" className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</a>
-            </div>
             <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
                 <div>
                     
                 </div>
                 <div>
                     <nav className="isolate inline-flex -space-x-px rounded-md shadow-xs" aria-label="Pagination">
-                        <a href="#" className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+                        <a onClick={decrement} href="#" className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
                             <span className="sr-only">Previous</span>
                             <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
                                 <path fill-rule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
                             </svg>
                         </a>
-                        <a href="#" className={getPageNumberCSS(1)}>1</a>
-                        <a href="#" className={getPageNumberCSS(2)}>2</a>
-                        <a href="#" className={getPageNumberCSS(3)}>3</a>
+                        {pageNumberDisplay()}
                         <a onClick={increment} href="#" className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
                             <span className="sr-only">Next</span>
                             <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
