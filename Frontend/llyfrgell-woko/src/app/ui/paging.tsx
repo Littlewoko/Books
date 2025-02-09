8c41ca99b2ec3d6fd91d48d21162089f5df9cc00
Bradley Watkins <watkinsbradley01@gmail.com>
2025-02-09 13:43:51 +0000

    paging in center on xs sceens

--- Frontend/llyfrgell-woko/src/app/ui/paging.tsx
+++ Frontend/llyfrgell-woko/src/app/ui/paging.tsx
@@ -1,28 +1,28 @@
 interface Props {
-    page: number, 
+    page: number,
     setPage: React.Dispatch<React.SetStateAction<number>>,
     pageLimit: number
 }
 
 export default function Paging({ page, setPage, pageLimit = 2 }: Props) {
     const decrement = () => {
-        if(page == 0) return;
+        if (page == 0) return;
 
         setPage(page - 1);
     }
-    
+
     const increment = () => {
-        if(page === pageLimit) return;
+        if (page === pageLimit) return;
 
         setPage(page + 1);
     }
 
     const getPageNumberCSS = (pageNumber: number) => {
-        if(pageNumber === page + 1) {
+        if (pageNumber === page + 1) {
             return "relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         }
 
-        return "relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex";
+        return "relative items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex";
     }
 
     const pageNumbers = (): number[] => {
@@ -32,17 +32,17 @@ export default function Paging({ page, setPage, pageLimit = 2 }: Props) {
             // If there are less than three pages, return all available pages
             return Array.from({ length: pageLimit }, (_, index) => index + 1);
         }
-    
+
         if (displayPage === 1) {
             // If the current page is the first page, return the first three pages
             return [1, 2, 3];
         }
-    
+
         if (displayPage === pageLimit + 1) {
             // If the current page is the last page, return the last three pages
             return [displayPage - 2, displayPage - 1, displayPage];
         }
-    
+
         // For all other cases, return the current page and its adjacent pages
         return [displayPage - 1, displayPage, displayPage + 1];
     }
@@ -50,32 +50,30 @@ export default function Paging({ page, setPage, pageLimit = 2 }: Props) {
     const pageNumberDisplay = () => {
         let arr = pageNumbers();
 
-        return arr.map(num => <a className={getPageNumberCSS(num)}>{num}</a>)
+        return arr.map(num => <button className={getPageNumberCSS(num)} onClick={() => setPage(num - 1)}>{num}</button>)
     }
 
     return (
-        <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-            <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-                <div>
-                    
-                </div>
-                <div>
-                    <nav className="isolate inline-flex -space-x-px rounded-md shadow-xs" aria-label="Pagination">
-                        <a onClick={decrement} href="#" className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-                            <span className="sr-only">Previous</span>
-                            <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                                <path fill-rule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
-                            </svg>
-                        </a>
-                        {pageNumberDisplay()}
-                        <a onClick={increment} href="#" className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-                            <span className="sr-only">Next</span>
-                            <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                                <path fill-rule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
-                            </svg>
-                        </a>
-                    </nav>
-                </div>
+        <div className="flex items-center justify-center sm:justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+            <div className="hidden sm:flex">
+
+            </div>
+            <div>
+                <nav className="isolate inline-flex -space-x-px rounded-md shadow-xs" aria-label="Pagination">
+                    <a onClick={decrement} href="#" className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+                        <span className="sr-only">Previous</span>
+                        <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                            <path fill-rule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
+                        </svg>
+                    </a>
+                    {pageNumberDisplay()}
+                    <a onClick={increment} href="#" className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+                        <span className="sr-only">Next</span>
+                        <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                            <path fill-rule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                        </svg>
+                    </a>
+                </nav>
             </div>
         </div>
     )
