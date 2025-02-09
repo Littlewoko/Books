f46881f0df1ed26bd7a90edc0e41570f90e16c91
Bradley Watkins <watkinsbradley01@gmail.com>
2025-02-09 14:25:59 +0000

    removed bg colour

--- Frontend/llyfrgell-woko/src/app/ui/paging.tsx
+++ Frontend/llyfrgell-woko/src/app/ui/paging.tsx
@@ -22,7 +22,7 @@ export default function Paging({ page, setPage, pageLimit = 2 }: Props) {
             return "relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         }
 
-        return "relative items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex";
+        return "relative items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset focus:z-20 focus:outline-offset-0 md:inline-flex";
     }
 
     const pageNumbers = (): number[] => {
@@ -54,25 +54,25 @@ export default function Paging({ page, setPage, pageLimit = 2 }: Props) {
     }
 
     return (
-        <div className="flex items-center justify-center sm:justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+        <div className="flex items-center justify-center sm:justify-between px-4 py-3 sm:px-6">
             <div className="hidden sm:flex">
 
             </div>
             <div>
                 <nav className="isolate inline-flex -space-x-px rounded-md shadow-xs" aria-label="Pagination">
-                    <a onClick={decrement} href="#" className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+                    <a onClick={decrement} className={`relative inline-flex items-center rounded-l-md px-2 py-2 ring-1 ring-gray-300 ring-inset focus:z-20 focus:outline-offset-0 ${page === 0 && 'text-gray-200'}`}>
                         <span className="sr-only">Previous</span>
                         <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
                             <path fill-rule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
                         </svg>
                     </a>
                     {pageNumberDisplay()}
-                    <a onClick={increment} href="#" className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+                    <button onClick={increment} className={`relative inline-flex items-center rounded-r-md px-2 py-2 ring-1 ring-gray-300 ring-inset focus:z-20 focus:outline-offset-0 ${page === pageLimit && 'text-gray-200'}`}>
                         <span className="sr-only">Next</span>
                         <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
                             <path fill-rule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
                         </svg>
-                    </a>
+                    </button>
                 </nav>
             </div>
         </div>
