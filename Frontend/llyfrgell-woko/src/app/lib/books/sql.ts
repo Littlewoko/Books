0ca4bfb7939bee2e085e2488a0ede31a54d1a710
Littlewoko <watkinsbradley01@gmail.com>
2025-07-15 19:17:08 +0100

    Search query

--- Frontend/llyfrgell-woko/src/app/lib/books/sql.ts
+++ Frontend/llyfrgell-woko/src/app/lib/books/sql.ts
@@ -1,24 +1,48 @@
 import { QueryResult, QueryResultRow, sql } from "@vercel/postgres";
 
-export async function GetBooksRequest(offset: number, limit: number): Promise<QueryResult<QueryResultRow>> {
-    return sql
-    `SELECT * FROM books
-    ORDER BY
-    CASE
-        WHEN dateCompleted IS NULL AND dateStartedReading IS NOT NULL THEN 1
-        WHEN dateCompleted IS NOT NULL THEN 2
-        WHEN dateCompleted IS NULL AND dateStartedReading IS NULL AND dateObtained IS NOT NULL THEN 3
-        ELSE 4
-    END,
-    CASE
-        WHEN dateCompleted IS NULL AND dateStartedReading IS NOT NULL THEN dateStartedReading
-        WHEN dateCompleted IS NOT NULL THEN dateCompleted
-        WHEN dateCompleted IS NULL AND dateStartedReading IS NULL AND dateObtained IS NOT NULL THEN dateObtained
-        ELSE NULL
-    END DESC 
-    OFFSET ${offset} LIMIT ${limit};`;
+export async function GetBooksRequest(
+    offset: number,
+    limit: number,
+    search?: string
+): Promise<QueryResult<QueryResultRow>> {
+    let query = `
+        SELECT * FROM books
+    `;
+    let params: (string | number)[] = [];
+
+    if (search) {
+        query += ` WHERE LOWER(title) LIKE $1`;
+        params.push(`%${search.toLowerCase()}%`);
+    }
+
+    query += `
+        ORDER BY
+            CASE
+                WHEN dateCompleted IS NULL AND dateStartedReading IS NOT NULL THEN 1
+                WHEN dateCompleted IS NOT NULL THEN 2
+                WHEN dateCompleted IS NULL AND dateStartedReading IS NULL AND dateObtained IS NOT NULL THEN 3
+                ELSE 4
+            END,
+            CASE
+                WHEN dateCompleted IS NULL AND dateStartedReading IS NOT NULL THEN dateStartedReading
+                WHEN dateCompleted IS NOT NULL THEN dateCompleted
+                WHEN dateCompleted IS NULL AND dateStartedReading IS NULL AND dateObtained IS NOT NULL THEN dateObtained
+                ELSE NULL
+            END DESC
+        OFFSET ${offset} LIMIT ${limit};
+    `;
+
+    if (search) {
+        return sql.query(query, params);
+    } else {
+        return sql.query(query);
+    }
 }
 
-export async function GetPageCountRequest(pageSize: number) {
-    return `SELECT CEILING(COUNT(*)/${pageSize}) FROM books`
+
+export async function GetPageCountRequest(pageSize: number, search?: string) {
+ const whereClause = search
+        ? "WHERE LOWER(title) LIKE '%" + search.toLowerCase() + "%'"
+        : "";
+    return "SELECT CEILING(COUNT(*)/" + pageSize + ") FROM books " + whereClause + ";";
 }
