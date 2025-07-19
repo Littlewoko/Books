261b1de4281b59beb49810b70aa6761c67bffc75
Littlewoko <watkinsbradley01@gmail.com>
2025-07-19 18:36:21 +0100

    linting i guess

--- Frontend/llyfrgell-woko/src/app/ui/authButton.tsx
+++ Frontend/llyfrgell-woko/src/app/ui/authButton.tsx
@@ -3,25 +3,19 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import LoginIcon from '@mui/icons-material/Login';
 
 export default function AuthButton() {
-    const { data: session, status } = useSession();
-
-    const isLoading = status === 'loading';
+    const { data: session } = useSession();
 
     return (
-        isLoading ?
+        session ?
+            <button onClick={() => signOut()} type="button" className="flex items-center text-white bg-gradient-to-r from-black to-gray-400 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-small rounded-lg text-sm p-1 px-2 md:px-3 text-center text-xs md:text-sm">
+                <LogoutIcon className="md:mr-1" fontSize="small" />
+                <span className="hidden md:inline">Allgofnodi</span>
+            </button>
+            :
             <button onClick={() => signIn()} type="button" className="flex items-center text-white bg-gradient-to-r from-black to-gray-400 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-small rounded-lg text-sm p-1 px-2 md:px-3 text-center text-xs md:text-sm">
-                <span className="inline">Loading...</span>
-            </button> :
-            status === 'authenticated' ?
-                <button onClick={() => signOut()} type="button" className="flex items-center text-white bg-gradient-to-r from-black to-gray-400 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-small rounded-lg text-sm p-1 px-2 md:px-3 text-center text-xs md:text-sm">
-                    <LogoutIcon className="md:mr-1" fontSize="small" />
-                    <span className="hidden md:inline">Allgofnodi</span>
-                </button>
-                :
-                <button onClick={() => signIn()} type="button" className="flex items-center text-white bg-gradient-to-r from-black to-gray-400 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-small rounded-lg text-sm p-1 px-2 md:px-3 text-center text-xs md:text-sm">
-                    <LoginIcon className="md:mr-1" fontSize="small" />
-                    <span className="hidden md:inline">Mewngofnodi</span>
-                </button>
+                <LoginIcon className="md:mr-1" fontSize="small" />
+                <span className="hidden md:inline">Mewngofnodi</span>
+            </button>
 
     )
 }
--- Frontend/llyfrgell-woko/src/app/ui/mobileAuthButton.tsx
+++ Frontend/llyfrgell-woko/src/app/ui/mobileAuthButton.tsx
@@ -3,9 +3,9 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import LoginIcon from '@mui/icons-material/Login';
 
 export default function MobileAuthButton() {
-    const { data: session, status } = useSession();
+    const { data: session } = useSession();
 
-    if (status === 'authenticated') {
+    if (session) {
         return (
             <button onClick={() => signOut()} type="button" className="w-44 flex justify-center flex items-center text-white bg-gradient-to-r from-black to-gray-400 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm p-1 p-3">
                 <LogoutIcon className="mr-1" fontSize="small" />
