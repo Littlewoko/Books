edd7b50999f0af7ae70d32303c7405466ca9e804
Bradley Watkins <2406413@student.uwtsd.ac.uk>
2025-08-03 15:43:01 +0100

    .

--- Frontend/llyfrgell-woko/src/app/ui/gradioSpace.tsx
+++ Frontend/llyfrgell-woko/src/app/ui/gradioSpace.tsx
@@ -10,7 +10,7 @@ export default function GradioSpace() {
                 src="https://gradio.s3-us-west-2.amazonaws.com/5.34.2/gradio.js"
             ></Script>
             <gradio-app 
-                src="https://littlewoko-llyfrgell-woko-dracula.hf.space"
+                space="https://littlewoko-llyfrgell-woko-dracula.hf.space"
                 style={{ 
                     width: '100%', // Take full width of its parent
                     height: '800px', // Set a fixed height
