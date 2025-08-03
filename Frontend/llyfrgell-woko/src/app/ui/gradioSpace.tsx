c93155ad3f9bbeddd2fb6af69659d0692e5e3c20
Bradley Watkins <2406413@student.uwtsd.ac.uk>
2025-08-03 15:57:06 +0100

    embedded iframe

--- Frontend/llyfrgell-woko/src/app/ui/gradioSpace.tsx
+++ Frontend/llyfrgell-woko/src/app/ui/gradioSpace.tsx
@@ -1,22 +1,10 @@
 'use client';
 
-import Script from "next/script";
-
 export default function GradioSpace() {
     return (
-        <>
-            <Script
-                type="module"
-                src="https://gradio.s3-us-west-2.amazonaws.com/5.34.2/gradio.js"
-            ></Script>
-            <gradio-app 
-                space="https://littlewoko-llyfrgell-woko-dracula.hf.space"
-                style={{ 
-                    width: '100%', // Take full width of its parent
-                    height: '800px', // Set a fixed height
-                    minHeight: '800px' // Ensure it doesn't collapse
-                }}
-            ></gradio-app>
-        </>
+        <iframe
+            src="https://littlewoko-llyfrgell-woko-dracula.hf.space"
+            className="w-full aspect-video min-h-[500px] p-1" 
+        ></iframe>
     );
 }
