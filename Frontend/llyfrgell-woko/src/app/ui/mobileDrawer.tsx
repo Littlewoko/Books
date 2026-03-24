16ac6d2d2f0213317dcf1acb86d1293841ec5928
Bradley Watkins <bradley.watkins@dvla.gov.uk>
2026-03-24 18:30:19 +0000

    fixed menu

--- Frontend/llyfrgell-woko/src/app/ui/mobileDrawer.tsx
+++ Frontend/llyfrgell-woko/src/app/ui/mobileDrawer.tsx
@@ -16,7 +16,7 @@ interface Props {
 const MobileDrawer: React.FC<Props> = ({ isOpen, onClose }) => {
   return (
     <div
-      className={`fixed flex flex-col justify-center items-center z-10 top-0 right-0 h-full w-full bg-white text-black transition-transform duration-300 transform ${isOpen ? 'translate-x-0' : 'translate-x-full'
+      className={`fixed flex flex-col justify-center items-center z-50 top-0 right-0 h-full w-full bg-white text-black transition-transform duration-300 transform ${isOpen ? 'translate-x-0' : 'translate-x-full'
         }`}
     >
       <button className="absolute right-4 top-4 p-3 text-black" onClick={onClose}>
